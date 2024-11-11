dfaefd5700dd875b85a1616e76beddf6bf022e51
Michael Michael <hallo@michaelfromage.com>
2024-11-11 20:14:22 +0100

    Project setup

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.cjs',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@': './src/',
+        },
+      },
+    ],
+  ],
+};
---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import pluginJs from '@eslint/js';
+import pluginImport from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'external',
+            'builtin',
+            'internal',
+            'sibling',
+            'parent',
+            'index',
+          ],
+          pathGroupsExcludedImportTypes: ['internal'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+  },
+];
